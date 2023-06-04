dda5e9a9cd3c01aeb4295946a0125967aae6b498
kmhbDOes <kmhbipul@gmail.com>
2023-06-04 22:40:09 +0600

    All Finished

--- index.js
+++ index.js
@@ -190,7 +190,7 @@ async function run() {
 
         app.post('/create-payment-intent', verifyJWT, async (req, res) => {
             const { price } = req.body;
-            const amount = price * 100;
+            const amount = parseInt(price * 100);
             const paymentIntent = await stripe.paymentIntents.create({
                 amount: amount,
                 currency: 'usd',
@@ -224,6 +224,41 @@ async function run() {
             const revenue = payments.reduce((sum, payment) => sum + payment.price, 0)
 
             res.send({ revenue, users, products, orders })
+        })
+
+        app.get('/order-stats', verifyJWT, verifyAdmin, async (req, res) => {
+            const pipeline = [
+                {
+                    $lookup: {
+                        from: 'menu',
+                        localField: 'menuItems',
+                        foreignField: '_id',
+                        as: 'menuItemsData'
+                    }
+                },
+                {
+                    $unwind: '$menuItemsData'
+                },
+                {
+                    $group: {
+                        _id: '$menuItemsData.category',
+                        count: { $sum: 1 },
+                        total: { $sum: '$menuItemsData.price' }
+                    }
+                },
+                {
+                    $project: {
+                        category: '$_id',
+                        count: 1,
+                        total: { $round: ['$total', 2] },
+                        _id: 0
+                    }
+                }
+            ];
+
+            const result = await paymentCollection.aggregate(pipeline).toArray()
+            res.send(result)
+
         })
 
         // Send a ping to confirm a successful connection
